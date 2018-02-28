c130bd23923cdc58a7678580264514c617de64ca
Mercado, Alfredo I. R <alfredo.i.r.mercado@accenture.com>
2018-02-28 14:50:46 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SpeechRecognitionService } from './speech-recognition.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        // angular builtin module
+        BrowserModule,
+        HttpModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+        SpeechRecognitionService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
